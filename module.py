b1dc72948d10b1a55bdf2e04cb925b7632f37652
artem-collab <58644376+artem-collab@users.noreply.github.com>
2019-12-22 02:20:48 +0400

    Create module.py

---
+++ module.py
@@ -0,0 +1,16 @@
+import os
+import time
+loser='''
+ m      mmmm   mmmmmm mmmmm
+ #        #        #" "m m"
+ #        #      m#    "#"
+ #        #     m"      #
+ #mmmmm mm#mm  ##mmmm   #
+'''
+print(loser)
+time.sleep(10)
+os.system('clear')
+kali='root@kali|> '
+while True:
+        tew=input(kali)
+        print('command not found')
